9f24ed024da38e7b9fce5d679e6fc40223cddfd1
Travis Chan <travismchan@gmail.com>
2018-08-03 16:20:09 +1000

    Working markdown previewer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import marked from 'marked';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value:
+        '# Header 1 \n## Header 2\n[I am an inline-style link](https://github.com/tchan) \n\nHello, `I am inline code` \n\n This is a code block \n```JavaScript\nvar s ="blah"```\n1. First Item\n2. Second item\n> Blockquotes\n\n**Image:**\n![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")'
     };
     this.handleChange = this.handleChange.bind(this);
   }
@@ -18,8 +20,12 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Markdown Previewer</h1>
         </header>
-        <TextArea value={this.state.value} onChange={this.handleChange} />
-        <Preview value={this.state.value} />
+        <div className='container'>
+          <div>
+          <TextArea value={this.state.value} onChange={this.handleChange} />
+          </div>
+          <Preview value={this.state.value} />
+        </div>
       </div>
     );
   }
@@ -34,15 +40,23 @@ class TextArea extends Component {
         rows="4"
         cols="50"
         value={this.props.value}
-        placeholder='Change me'
+        placeholder="Change me"
       />
     );
   }
 }
 
 class Preview extends Component {
+  componentDidMount() {
+    const markdown = marked(this.props.value);
+    document.getElementById('preview').innerHTML = markdown;
+  }
+  componentDidUpdate() {
+    const markdown = marked(this.props.value);
+    document.getElementById('preview').innerHTML = markdown;
+  }
   render() {
-    return <div id="preview">{this.props.value}</div>;
+    return <div id="preview" />;
   }
 }
 
